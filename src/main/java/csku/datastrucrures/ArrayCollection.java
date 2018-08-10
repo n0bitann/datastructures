c286eb6fdfa573801fa3c6064f01ff5df46dbdad
Tan Rutthanawin <thun18@gmail.com>
2018-08-11 01:38:03 +0700

    initial commit

---
+++ src/main/java/csku/datastrucrures/ArrayCollection.java
@@ -0,0 +1,23 @@
+package csku.datastrucrures;
+
+public class ArrayCollection implements Collection {
+    public void add(Object element) {
+
+    }
+
+    public void remove(Object element) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(Object element) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
---
+++ src/main/java/csku/datastrucrures/Collection.java
@@ -0,0 +1,11 @@
+package csku.datastrucrures;
+
+public interface Collection {
+    void add(Object element);
+    void remove(Object element);
+    boolean isEmpty();
+    boolean contains(Object element);
+    int size();
+
+
+}
