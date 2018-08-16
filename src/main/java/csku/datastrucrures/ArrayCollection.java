c7fcda62fe45e5749d5b312e688283c8560af085
Tan Rutthanawin <thun18@gmail.com>
2018-08-17 00:49:41 +0700

    implement  all method

--- src/main/java/csku/datastrucrures/ArrayCollection.java
+++ src/main/java/csku/datastrucrures/ArrayCollection.java
@@ -1,23 +1,50 @@
 package csku.datastrucrures;
 
 public class ArrayCollection implements Collection {
+    private Object[] objs;
+    private int size;
+
+
+    public ArrayCollection(int c){
+        this.objs = new Object[c];
+        this.size = 0;
+        
+    }
     public void add(Object element) {
+        if (element == null){
+            throw new IllegalArgumentException();
+        }
+        this.objs[size++] = element;
 
     }
 
     public void remove(Object element) {
-
+        
+        int i = indexOf(element);
+        if (i != -1){
+            this.objs[i] = this.objs[--i];
+            this.objs[size] = null;
+            
+        }
     }
 
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     public boolean contains(Object element) {
-        return false;
+        return indexOf(element) != -1; 
     }
 
     public int size() {
-        return 0;
+        return size;
+    }
+
+    private int indexOf(Object element){
+        
+      for (int i =0 ;i<size;i++){
+          if(this.objs[i].equals(element)) return i;
+      }
+      return -1; 
     }
 }
